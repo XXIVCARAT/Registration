c553aeed449af7a69d48a62f31ca6b8b6e159b12
XXIVCARAT <106384872+XXIVCARAT@users.noreply.github.com>
2022-06-02 14:45:15 +0530

    Add files via upload

--- adminLogIn.php
+++ adminLogIn.php
@@ -16,28 +16,28 @@
         $pswd = stripslashes($_REQUEST['pswd']);
         $pswd = mysqli_real_escape_string($con, $pswd);
         // Check user is exist in the database
-        $query    = "SELECT * FROM `registration` WHERE pswd='" . md5($pswd) . "' AND email='$email'";
+        $query    = "SELECT * FROM `admin` WHERE pswd='" . md5($pswd) . "' AND email='$email'";
         $result = mysqli_query($con, $query) or die(mysqli_error($conn));
         $rows = mysqli_num_rows($result);
         if ($rows == 1) {
             $_SESSION['email'] = $email;
             // Redirect to user dashboard page
-            header("Location: dashboard.php");
+            header("Location: adminDashBoard.php");
         } else {
             echo "<div class='form'>
                   <h3>Incorrect email/password.</h3><br/>
-                  <p class='link'>Click here to <a href='login.php'>Login</a> again.</p>
+                  <p class='link'>Click here to <a href='adminLogIn.php'>Login</a> again.</p>
                   </div>";
         }
     } else {
 ?>
     <form class="form" method="post" name="login">
-        <h1 class="login-title">Student Login</h1>
+        <h1 class="login-title">Admin Login</h1>
         <input type="email" class="login-input" name="email" placeholder="Email Address" autofocus="true"/>
         <input type="password" class="login-input" name="pswd" placeholder="Password"/>
         <input type="submit" value="Login" name="submit" class="login-button"/>
         <p class="link"><a href="register.php">New Registration</a></p>
-        <p class="link"><a href="adminLogIn.php">Admin Login</a></p>
+        <p class="link"><a href="login.php">Student Login</a></p>
   </form>
 <?php
     }
--- register.php
+++ register.php
@@ -1,116 +1,111 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Registration</title>
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
 <?php
-$insert = false;
+require "db_connection.php";
+// When form submitted, insert values into the database.
+if (isset($_REQUEST["email"])) {
+    // removes backslashes
+    $name = stripslashes($_REQUEST["name"]);
+    //escapes special characters in a string
+    $name = mysqli_real_escape_string($con, $name);
+    $age = stripslashes($_REQUEST["age"]);
+    $age = mysqli_real_escape_string($con, $age);
+    $school = stripslashes($_REQUEST["school"]);
+    $school = mysqli_real_escape_string($con, $school);
+    $email = stripslashes($_REQUEST["email"]);
+    $email = mysqli_real_escape_string($con, $email);
+    $phone = stripslashes($_REQUEST["phone"]);
+    $phone = mysqli_real_escape_string($con, $phone);
+    $pswd = stripslashes($_REQUEST["pswd"]);
+    $pswd = mysqli_real_escape_string($con, $pswd);
+    $cpswd = stripslashes($_REQUEST["cpswd"]);
+    $cpswd = mysqli_real_escape_string($con, $cpswd);
+    if ($pswd != $cpswd) {
+        echo '<script type="text/javascript">
+                window.onload = function () { 
+                    alert("Password Doesnt Match");
+                    
+                } 
+            </script>';
+        echo "<div class='form'>
+                  
+                  <p class='link'>Click here to <a href='register.php'>register again</a> again.</p>
+                  </div>";
+    } else {
+        $uppercase = preg_match("@[A-Z]@", $pswd);
+        $lowercase = preg_match("@[a-z]@", $pswd);
+        $number = preg_match("@[0-9]@", $pswd);
+        $specialChars = preg_match("@[^\w]@", $pswd);
+        if ($uppercase || $lowercase || $number || $specialChars || strlen($password) > 8) {
+            $checkEmail = mysqli_query($con, "SELECT * FROM exam.registration WHERE email = '" . $_POST["email"] . "'");
+            $checkPhone = mysqli_query($con, "SELECT * FROM exam.registration WHERE phone = '" . $_POST["phone"] . "'");
+            if (mysqli_num_rows($checkEmail) != 0 || mysqli_num_rows($checkPhone) != 0) {
+                echo '<script type="text/javascript">
+                window.onload = function () { 
+                    alert("Phone Number OR Email Id Already Exists");
+                    
+                } 
+            </script>';
 
-if(isset($_POST['name'], $_POST['email'], $_POST['pswd'], $_POST['cpswd'])){
-            if($_POST['pswd']!=$_POST['cpswd']) {
-echo"Passwords don't match";
+            echo "<div class='form'>
+                  
+            <p class='link'>Click here to <a href='register.php'>register again</a> again.</p>
+            </div>";
+            
+            } else {
+                $query = "INSERT into `registration` (name, age, school, email, phone, pswd, dt)
+                     VALUES ('$name', '$age', '$school', '$email', '$phone', '" . md5($pswd) . "', current_timestamp());";
+                $result = mysqli_query($con, $query);
+                if ($result) {
+                    echo "<div class='form'>
+                  <h3>You are registered successfully.</h3><br/>
+                  <p class='link'>Click here to <a href='login.php'>Login</a></p>
+                  </div>";
+                } else {
+                    echo "<div class='form'>
+                  <h3>Required fields are missing.</h3><br/>
+                  <p class='link'>Click here to <a href='registration.php'>registration</a> again.</p>
+                  </div>";
+                }
             }
-            else{
-   
-//set connection variables
-$server = "localhost";
-$username = "root";
-$password = "";
+        } else {
+            echo '<script type="text/javascript">
+                window.onload = function () { 
+                    alert("Password Should contain atleast 1 Special Character,Number,Upper and Lowercase Letter And Should be greater than 8 character");
+                    
+                } 
+            </script>';
 
-//Create a Database connection
-$con = mysqli_connect($server,$username,$password);
-//if connection success
-if(!$con){
-    die("connection to this database failed due to " . mysqli_connect_error());
-}
-
-// echo "Successful Connection";
-
-$name = strip_tags($_POST['name']);
-$age = strip_tags($_POST['age']);
-$school =strip_tags( $_POST['school']);
-$email = strip_tags($_POST['email']);
-$phone = strip_tags($_POST['phone']);
-$desc = strip_tags($_POST['desc']);
-$pswd = strip_tags($_POST['pswd']);
-$cpswd = strip_tags($_POST['cpswd']);
-
-//query to pull rows which have email id equal to entered email id
-$queryCheckEmail = mysqli_query($con ,"SELECT * FROM trip.trip WHERE email = '".$_POST['email']."'"); 
-
-//query to pull rows which have phone number equal to entered email id
-$queryCheckPhone = mysqli_query($con ,"SELECT * FROM trip.trip WHERE phone = '".$_POST['phone']."'");
-
-  if (mysqli_num_rows($queryCheckEmail) != 0) //if no. of rows with emailid != 0 ; if statement executed
-  {
-      echo "Email Id already exists";
-  }
-  
-  else if (mysqli_num_rows($queryCheckPhone) != 0) //if no. of rows with phone != 0 ; if statement executed
-  {
-      echo "Phone Number already exists";
-  }
-
-  else //only executed when there is no duplicate email and phone no.
-  {
-             $sql = " INSERT INTO `trip`.`trip` (`name`, `age`, `school`, `email`, `phone`, `other`, `pswd`,`cpswd`,`dt`) 
-                     VALUES ('$name', '$age', '$school', '$email', '$phone', '$desc', '$pswd', '$cpswd', current_timestamp());";
-                if($con -> query($sql)==true){
-                    // echo "Succesfully Inserted";
-                 
-                    // Flag for successful insertion
-                    $insert = true;
-                 }
-                 else{
-                     echo "ERROR : $sql <br> $con ->error";
-                 }
-                 
-                     // Close the database connection
-                 $con -> close();
-                 }
-
-                 echo "You have successfuly registered";
-
-}
+            echo "<div class='form'>
+                  
+            <p class='link'>Click here to <a href='register.php'>register again</a> again.</p>
+            </div>";
+        }
+    }
+} else {
+?>
+    <form class="form" action="" method="post">
+        <h1 class="login-title">Registration</h1>
+        <input type="text" class="login-input" name="name" placeholder="Name" required />
+        <input type="date" class="login-input" name="age" placeholder= "Enter your Date Of Birth">
+        <input type="text" class="login-input" name="email" placeholder="Email Adress" required>
+        <input type="phone" class="login-input" name="phone" placeholder="Phone Number" required>
+        <input type="text" class="login-input" name="school" placeholder="Enter your School Name">
+        <input type="password" class="login-input" name="pswd" placeholder="Password">
+        <input type="password" class="login-input" name="cpswd" placeholder="Confirm Password">
+        <input type="submit" name="submit" value="Register" class="login-button">
+        <p class="link"><a href="login.php">Click to Login</a></p>
+    </form>
+    
+<?php
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome To Login Page</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
-
-<body>
-    <!-- Added later for nav bar -->
-<?php
-        require 'partials/_nav.php'
-    ?>
-
-    <div class="container">
-        <h1>STUDENT EXAM REGISTRATION PORTAL</h1>
-        <P>
-            Enter your details to access exam portal
-        </P>
-
-        <form action="register.php" method="post">
-            <input type="text" name="name" id="name" placeholder="Enter your Name">
-            <input type="date" name="age" id="age" placeholder= "Enter your Date Of Birth">
-            <input type="email" name="email" id="email" placeholder="Enter your Email Address">
-            <input type="phone" name="phone" id="phone" placeholder="Enter your Phone Number">
-            <input type="text" name="school" id="school" placeholder="Enter your School Name">
-            <textarea name="desc" id="desc" cols="30" rows="10" placeholder="Enter your School Address">
-</textarea>
-<input type="password" name="pswd" id="pswd" placeholder="Please choose a password">
-<input type="password" name="cpswd" id="cpswd" placeholder="Re-type to confirm your password">
-<button class="submit">Submit</button>
-
-        </form>
-    </div>
-    <script src="index.js"></script>
-
-
 </body>
-
 </html>
